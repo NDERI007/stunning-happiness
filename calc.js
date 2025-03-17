8633169097c20e32bfc9d58703ff92e9cace9603
NDERI007 <tnderi373@gmail.com>
2025-03-17 13:30:07 +0300

    project complete bugs unknown at the time of writing

--- calc.js
+++ calc.js
@@ -2,19 +2,27 @@ let currentNum = "";
 let previousNum = "";
 let operator = "";
 
+window.addEventListener("keydown", handleKEyPress);
+
 const currentDispnUM = document.querySelector(".currentNumber");
 const prevDispNum = document.querySelector(".previousNumber");
 
 const equal = document.querySelector(".equal");
-equal.addEventListener("click", calculate);
-
+equal.addEventListener("click", () => {
+    if (currentNum != "" && previousNum != "") {
+        calculate();
+    }
+});
 
 const decbtn = document.querySelector(".decimal");
+decbtn.addEventListener("click", () => {
+    addDEc();
+});
 
 const operatorBTn = document.querySelectorAll(".operator");
 
 const clear = document.querySelector(".clear");
-
+clear.addEventListener("click", clearCalc);
 
 const nUMbtN = document.querySelectorAll(".number");
 
@@ -25,6 +33,10 @@ nUMbtN.forEach((btn) => {
 });
 
 function handleNumber(number) {
+    if (currentNum != "" && previousNum != "" && operator === "") {
+        previousNum = "";
+        currentDispnUM.textContent = currentNum;
+    }
     if (currentNum.length <= 12) {
     currentNum += number;
     currentDispnUM.textContent = currentNum;
@@ -38,11 +50,24 @@ operatorBTn.forEach((btn) => {
 });
 
 function handleOper(op) {
-    operator += op;
-   previousNum = currentNum;
-   prevDispNum.textContent = previousNum + " " + operator;
-   currentNum = "";
-   currentDispnUM.textContent = "";
+   if (previousNum === "") {
+       previousNum = currentNum;
+       operatorCheck(op);
+   } else if (currentNum === "") {
+       operatorCheck(op);
+   }  else{
+      calculate();
+      operator = op;
+      prevDispNum.textContent = previousNum + " " + operator;
+      currentDispnUM.textContent = "0";
+   }
+}
+
+function operatorCheck(text) {
+    operator = text;
+    prevDispNum.textContent = previousNum + " " + operator;
+    currentDispnUM.textContent = "0";
+    currentNum = "";
 }
 
 function calculate() {
@@ -75,11 +100,65 @@ function RoundNum(num) {
     return Math.round(num * 100000) / 100000;
 }
 function displayResult() {
-    prevDispNum.textContent = "";
-    operator = "";
     if (previousNum.length < 11) {
         currentDispnUM.textContent = previousNum;
     } else {
-        currentDispnUM.textContent = previousNum.slice(0, 11) + "..."
+        currentDispnUM.textContent = previousNum.slice(0, 11) + "...";
+    }
+    prevDispNum.textContent = "";
+    operator = "";
+    currentNum = "";
+}
+
+function clearCalc() {
+    previousNum = "";
+    currentNum = "";
+    operator = "";
+    currentDispnUM.textContent = "";
+    prevDispNum.textContent = "";
+}
+
+function addDEc() {
+    if (!currentNum.includes(".")) {
+        currentNum += ".";
+        currentDispnUM.textContent = currentNum;
+    }
+}
+function handleKEyPress(e) {
+    e.preventDefault()
+    if (e.key >= 0 && e.key <= 9) {
+        handleNumber(e.key)
+    }
+    if (
+        e.key === "Enter" || 
+        (e.key === "=" && previousNum != "" && currentNum != "")
+     ) {
+        calculate();
+    }
+    if (e.key === "-" || e.key === "+" || e.key === "/") {
+        handleOper(e.key);
+    }
+    if (e.key === "*") {
+        handleOper(e.key);
+    }
+    if (e.key === ".") {
+        addDEc(e.key);
+    }
+    if (e.key === "Backspace") {
+        HandlDel();
+    }
+}
+
+function HandlDel() {
+    if (currentNum != "") {
+        currentNum = currentNum.slice(0, -1);
+        currentDispnUM.textContent = currentNum;
+    }
+    if (currentNum === "") {
+        currentNum = "0";
+    }
+    if (currentNum === "" && previousNum !== "" && operator === "") {
+        previousNum = previousNum.slice(0, -1);
+        currentDispnUM.textContent = previousNum;
     }
 }
